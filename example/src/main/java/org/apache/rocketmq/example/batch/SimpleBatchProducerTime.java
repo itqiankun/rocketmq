fe3446e79f5e1da2c43ef5f2b67128c3a8951c69
ma_qiankun <ma_qiankun@hzlh.com>
2023-07-09 09:07:25 +0800

    测试本地推送消息时间

--- example/src/main/java/org/apache/rocketmq/example/batch/SimpleBatchProducerTime.java
+++ example/src/main/java/org/apache/rocketmq/example/batch/SimpleBatchProducerTime.java
@@ -17,29 +17,47 @@
 
 package org.apache.rocketmq.example.batch;
 
+import com.google.common.base.Stopwatch;
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-public class SimpleBatchProducer {
+public class SimpleBatchProducerTime {
+    private static final Logger log = LoggerFactory.getLogger(SimpleBatchProducerTime.class);
 
     public static void main(String[] args) throws Exception {
         DefaultMQProducer producer = new DefaultMQProducer("BatchProducerGroupName");
         producer.setNamesrvAddr("127.0.0.1:9876");
         producer.start();
-        //If you just send messages of no more than 1MiB at a time, it is easy to use batch
-        //Messages of the same batch should have: same topic, same waitStoreMsgOK and no schedule support
-        String topic = "BatchTest1";
+        // 这个字符串大概1kb
         String str="{\"code\":200,\"data\":{\"similarList\":[],\"success\":[{\"image\":\"https://fos.fjecloud.com:8888/webdav//data/img/20230708/orgId22af2f501a044e5db001d1a5de2f2ede/videoId1/1677708917364228096.jpg\",\"videoValidation\":\"3481\",\"bbox_list\":[],\"kwargs\":{\"displayAllBboxes\":true,\"coldActivation\":{\"includeClasses\":\"[]\",\"type\":\"\"},\"bigModelQueryPositive\":[],\"displayScore\":true,\"classesNeeded\":\"[]\",\"boxEnlargeSize\":\"1\",\"yBMZhaRqXOXBJmjJ\":0.5,\"bigModelIncludeCoincideRatio\":[],\"isStart\":false,\"polygon_regions\":[],\"man_count\":{\"person_num\":0.0},\"includeCoincideRatio\":\"[]\",\"polygon_regions_label\":[],\"num_background\":0.0,\"LC_CP_num_person_thresh\":1.0,\"encodingFilter\":{\"indexId\":\"od210_210_001835_008_rfjs5fhx\",\"threshold\":0.9,\"videoId\":27897.0,\"orgId\":\"mu_100195\"},\"startTime\":\"00:00:00\",\"interval\":5.0,\"endTime\":\"23:59:59\",\"bigModelQueryNegative\":[],\"excludeCoincideRatio\":\"[]\",\"bigModelExcludeCoincideRatio\":[],\"classesThreshold\":0.5,\"num_alerts\":0.0},\"description\":\"注意！当前画面出现告警，请关注！\",\"id\":\"101630712\",\"status\":true}],\"error\":[]},\"appSourceId\":\"1677709120012025856\",\"taskId\":\"1258\"}";
-        List<Message> messages = new ArrayList<>();
+        for (int i = 1; i < 1000; i++) {
+            sendMessageTime(i, producer, str);
+        }
+        sendMessageTime(1, producer, str);
+        producer.shutdown();
+    }
 
-        for (int i = 0; i < 1000; i++) {
+    public static void sendMessageTime(int retry, DefaultMQProducer producer, String str){
+        String topic = "BatchTest1";
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        List<Message> messages = new ArrayList<>();
+        for (int i = 0; i < retry; i++) {
             messages.add(new Message(topic, "Tag", "OrderID00"+i, str.getBytes()));
         }
-        producer.send(messages);
-        producer.shutdown();
-
+        try {
+            producer.send(messages);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        log.info("{}消息发送毫秒时间:{}", messages.size(),stopwatch.elapsed(TimeUnit.MILLISECONDS));
     }
 }
--- example/src/main/java/org/apache/rocketmq/example/quickstart/Producer.java
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/Producer.java
@@ -21,94 +21,70 @@ import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * This class demonstrates how to send messages to brokers using provided {@link DefaultMQProducer}.
  */
 public class Producer {
+    private static final Logger log = LoggerFactory.getLogger(Producer.class);
+
     public static void main(String[] args) throws MQClientException, InterruptedException {
 
         /*
          * Instantiate with a producer group name.
          */
         DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
-
-        /*
-         * Specify name server addresses.
-         * <p/>
-         *
-         * Alternatively, you may specify name server addresses via exporting environmental variable: NAMESRV_ADDR
-         * <pre>
-         * {@code
-         * producer.setNamesrvAddr("name-server1-ip:9876;name-server2-ip:9876");
-         * }
-         * </pre>
-         */
-
-        /*
-         * Launch the instance.
-         */
+        producer.setNamesrvAddr("127.0.0.1:9876");
         producer.start();
 
+        final CountDownLatch countDownLatch = new CountDownLatch(10000); // 相当于计数器，当所有都准备好了，再一起执行，模仿多并发，保证并发量
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1000, 1000, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+        try {
+            for (int i = 0; i <= 10000; i++) {
+                threadPoolExecutor.submit(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            countDownLatch.await();
+                            sendMessage(producer);
+                            log.info("thread:{} end.", Thread.currentThread().getName());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+                countDownLatch.countDown();
+            }
+            threadPoolExecutor.shutdown();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        producer.shutdown();
+    }
+
+    private static void sendMessage(DefaultMQProducer producer) throws InterruptedException {
         for (int i = 0; i < 1000; i++) {
             try {
-
-                /*
-                 * Create a message instance, specifying topic, tag and message body.
-                 */
                 Message msg = new Message("TopicTest" /* Topic */,
                     "TagA" /* Tag */,
                     ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
                 );
-
-                /*
-                 * Call send message to deliver message to one of brokers.
-                 */
                 SendResult sendResult = producer.send(msg);
-                /*
-                 * There are different ways to send message, if you don't care about the send result,you can use this way
-                 * {@code
-                 * producer.sendOneway(msg);
-                 * }
-                 */
-
-                /*
-                 * if you want to get the send result in a synchronize way, you can use this send method
-                 * {@code
-                 * SendResult sendResult = producer.send(msg);
-                 * System.out.printf("%s%n", sendResult);
-                 * }
-                 */
-
-                /*
-                 * if you want to get the send result in a asynchronize way, you can use this send method
-                 * {@code
-                 *
-                 *  producer.send(msg, new SendCallback() {
-                 *  @Override
-                 *  public void onSuccess(SendResult sendResult) {
-                 *      // do something
-                 *  }
-                 *
-                 *  @Override
-                 *  public void onException(Throwable e) {
-                 *      // do something
-                 *  }
-                 *});
-                 *
-                 *}
-                 */
-
-                System.out.printf("%s%n", sendResult);
+//                System.out.printf("%s%n", sendResult);
             } catch (Exception e) {
                 e.printStackTrace();
-                Thread.sleep(1000);
             }
         }
-
-        /*
-         * Shut down once the producer instance is not longer in use.
-         */
-        producer.shutdown();
     }
 }
