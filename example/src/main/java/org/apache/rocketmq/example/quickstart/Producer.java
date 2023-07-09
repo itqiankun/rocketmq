e323e99aa1de6d200fee82e06bcf52a5f8fdb8f7
ma_qiankun <ma_qiankun@hzlh.com>
2023-07-09 09:07:25 +0800

    测试本地推送消息时间

--- example/src/main/java/org/apache/rocketmq/example/quickstart/Producer.java
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/Producer.java
@@ -21,105 +21,70 @@ import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * This class demonstrates how to send messages to brokers using provided {@link DefaultMQProducer}.
  */
 public class Producer {
-
-    /**
-     * The number of produced messages.
-     */
-    public static final int MESSAGE_COUNT = 1000;
-    public static final String PRODUCER_GROUP = "please_rename_unique_group_name";
-    public static final String DEFAULT_NAMESRVADDR = "127.0.0.1:9876";
-    public static final String TOPIC = "TopicTest";
-    public static final String TAG = "TagA";
+    private static final Logger log = LoggerFactory.getLogger(Producer.class);
 
     public static void main(String[] args) throws MQClientException, InterruptedException {
 
         /*
          * Instantiate with a producer group name.
          */
-        DefaultMQProducer producer = new DefaultMQProducer(PRODUCER_GROUP);
-
-        /*
-         * Specify name server addresses.
-         *
-         * Alternatively, you may specify name server addresses via exporting environmental variable: NAMESRV_ADDR
-         * <pre>
-         * {@code
-         *  producer.setNamesrvAddr("name-server1-ip:9876;name-server2-ip:9876");
-         * }
-         * </pre>
-         */
-        // Uncomment the following line while debugging, namesrvAddr should be set to your local address
-        producer.setNamesrvAddr(DEFAULT_NAMESRVADDR);
-
-        /*
-         * Launch the instance.
-         */
+        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+        producer.setNamesrvAddr("127.0.0.1:9876");
         producer.start();
 
-        for (int i = 0; i < MESSAGE_COUNT; i++) {
-            try {
+        final CountDownLatch countDownLatch = new CountDownLatch(10000); // 相当于计数器，当所有都准备好了，再一起执行，模仿多并发，保证并发量
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1000, 1000, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+        try {
+            for (int i = 0; i <= 10000; i++) {
+                threadPoolExecutor.submit(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            countDownLatch.await();
+                            sendMessage(producer);
+                            log.info("thread:{} end.", Thread.currentThread().getName());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
 
-                /*
-                 * Create a message instance, specifying topic, tag and message body.
-                 */
-                Message msg = new Message(TOPIC /* Topic */,
-                    TAG /* Tag */,
+                    }
+                });
+                countDownLatch.countDown();
+            }
+            threadPoolExecutor.shutdown();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        producer.shutdown();
+    }
+
+    private static void sendMessage(DefaultMQProducer producer) throws InterruptedException {
+        for (int i = 0; i < 1000; i++) {
+            try {
+                Message msg = new Message("TopicTest" /* Topic */,
+                    "TagA" /* Tag */,
                     ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
                 );
-
-                /*
-                 * Call send message to deliver message to one of brokers.
-                 */
                 SendResult sendResult = producer.send(msg);
-                /*
-                 * There are different ways to send message, if you don't care about the send result,you can use this way
-                 * {@code
-                 * producer.sendOneway(msg);
-                 * }
-                 */
-
-                /*
-                 * if you want to get the send result in a synchronize way, you can use this send method
-                 * {@code
-                 * SendResult sendResult = producer.send(msg);
-                 * System.out.printf("%s%n", sendResult);
-                 * }
-                 */
-
-                /*
-                 * if you want to get the send result in a asynchronize way, you can use this send method
-                 * {@code
-                 *
-                 *  producer.send(msg, new SendCallback() {
-                 *  @Override
-                 *  public void onSuccess(SendResult sendResult) {
-                 *      // do something
-                 *  }
-                 *
-                 *  @Override
-                 *  public void onException(Throwable e) {
-                 *      // do something
-                 *  }
-                 *});
-                 *
-                 *}
-                 */
-
-                System.out.printf("%s%n", sendResult);
+//                System.out.printf("%s%n", sendResult);
             } catch (Exception e) {
                 e.printStackTrace();
-                Thread.sleep(1000);
             }
         }
-
-        /*
-         * Shut down once the producer instance is no longer in use.
-         */
-        producer.shutdown();
     }
 }
